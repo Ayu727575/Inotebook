f68bc86fa22135062dc0cbe72f5d4734903f3f43
ayush <ayush@chouksey.com>
2024-01-18 11:05:05 +0530

    Lecture 62 - Adding font awesome icons to iNotebook

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -4,7 +4,7 @@ import NoteContext from "./noteContext";
 const NoteState = (props) => {
   const notesInitial = [
     {
-      _id: "65a609c793a88290a25540b8",
+      _id: "65a609c793a8h540b88",
       user: "659f97231f01ddf02486e56d",
       title: "dsa prepration",
       description:
@@ -14,7 +14,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "65a786bbc781b460908c6437",
+      _id: "6578g1b460908c64337",
       user: "659f97231f01ddf02486e56d",
       title: "dsa prepration 2",
       description:
@@ -24,7 +24,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "65a609c793a88290a25540b8",
+      _id: "65a609c88290a255440b8",
       user: "659f97231f01ddf02486e56d",
       title: "dsa prepration",
       description:
@@ -34,7 +34,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "65a786bbc781b460908c6437",
+      _id: "65a786e0908cc6437",
       user: "659f97231f01ddf02486e56d",
       title: "dsa prepration 2",
       description:
@@ -44,7 +44,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "65a609c793a88290a25540b8",
+      _id: "65a60d3a88290a255540b8",
       user: "659f97231f01ddf02486e56d",
       title: "dsa prepration",
       description:
@@ -54,7 +54,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "65a786bbc781b460908c6437",
+      _id: "65a78c6b781b4609008c6437",
       user: "659f97231f01ddf02486e56d",
       title: "dsa prepration 2",
       description:
@@ -64,7 +64,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "65a609c793a88290a25540b8",
+      _id: "65a6b09c793a88290a22554",
       user: "659f97231f01ddf02486e56d",
       title: "dsa prepration",
       description:
@@ -74,7 +74,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "65a786bbc781b460908c6437",
+      _id: "65aa786bbc781bb46",
       user: "659f97231f01ddf02486e56d",
       title: "dsa prepration 2",
       description:
