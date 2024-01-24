23a6e3a558eca1a927ab35396f41e84ad742d668
ayush <ayush@chouksey.com>
2024-01-24 15:24:14 +0530

    Lecture 65 - Adding 'fetch notes' functionality to iNotebook

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,11 +1,15 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import NoteContext from "../context/notes/noteContext";
 import Noteitem from "./Noteitem";
 import AddNote from "./AddNote";
 
 const Notes = () => {
   const context = useContext(NoteContext);
-  const { notes, addNote } = context;
+  const { notes, getNotes } = context;
+  useEffect(() => {
+    getNotes();
+  }, []);
+
   return (
     <>
       <AddNote />
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,105 +2,53 @@ import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-  const notesInitial = [
-    {
-      _id: "65a609c793a8h540b88",
-      user: "659f97231f01ddf02486e56d",
-      title: "dsa prepration",
-      description:
-        "dsa is very important topic for grabing job in product based company",
-      tag: "personal",
-      date: "2024-01-16T04:44:55.428Z",
-      __v: 0,
-    },
-    {
-      _id: "6578g1b460908c64337",
-      user: "659f97231f01ddf02486e56d",
-      title: "dsa prepration 2",
-      description:
-        "dsa is very important topic for grabing job in product based company 2",
-      tag: "personal",
-      date: "2024-01-17T07:50:19.520Z",
-      __v: 0,
-    },
-    {
-      _id: "65a609c88290a255440b8",
-      user: "659f97231f01ddf02486e56d",
-      title: "dsa prepration",
-      description:
-        "dsa is very important topic for grabing job in product based company",
-      tag: "personal",
-      date: "2024-01-16T04:44:55.428Z",
-      __v: 0,
-    },
-    {
-      _id: "65a786e0908cc6437",
-      user: "659f97231f01ddf02486e56d",
-      title: "dsa prepration 2",
-      description:
-        "dsa is very important topic for grabing job in product based company 2",
-      tag: "personal",
-      date: "2024-01-17T07:50:19.520Z",
-      __v: 0,
-    },
-    {
-      _id: "65a60d3a88290a255540b8",
-      user: "659f97231f01ddf02486e56d",
-      title: "dsa prepration",
-      description:
-        "dsa is very important topic for grabing job in product based company",
-      tag: "personal",
-      date: "2024-01-16T04:44:55.428Z",
-      __v: 0,
-    },
-    {
-      _id: "65a78c6b781b4609008c6437",
-      user: "659f97231f01ddf02486e56d",
-      title: "dsa prepration 2",
-      description:
-        "dsa is very important topic for grabing job in product based company 2",
-      tag: "personal",
-      date: "2024-01-17T07:50:19.520Z",
-      __v: 0,
-    },
-    {
-      _id: "65a6b09c793a88290a22554",
-      user: "659f97231f01ddf02486e56d",
-      title: "dsa prepration",
-      description:
-        "dsa is very important topic for grabing job in product based company",
-      tag: "personal",
-      date: "2024-01-16T04:44:55.428Z",
-      __v: 0,
-    },
-    {
-      _id: "65aa786bbc781bb46",
-      user: "659f97231f01ddf02486e56d",
-      title: "dsa prepration 2",
-      description:
-        "dsa is very important topic for grabing job in product based company 2",
-      tag: "personal",
-      date: "2024-01-17T07:50:19.520Z",
-      __v: 0,
-    },
-  ];
+  const HOST = "http://localhost:5000";
+  const notesInitial = [];
   const [notes, setNotes] = useState(notesInitial);
 
-  // Add Note
-  const addNote = (title, description, tag) => {
+  // Get All Notes
+  const getNotes = async () => {
     //TODO api call
-    console.log("adding a new note");
-    const note = {
-      _id: "65aa786bbc781bb46[added]",
-      user: "659f97231f01ddf02486e56d",
-      title: title,
-      description: description,
-      tag: tag,
-      date: "2024-01-17T07:50:19.520Z",
-      __v: 0,
-    };
-    setNotes(notes.concat(note));
+    const response = await fetch(`${HOST}/api/notes/fetchallnotes/`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token":
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjU5Zjk3MjMxZjAxZGRmMDI0ODZlNTZkIn0sImlhdCI6MTcwNDk3NDEwMn0.3NWEnroW3nHsCsAQnPlVBAcn37PKQx182KdO7dO40JI",
+      },
+    });
+    const json = await response.json();
+    console.log(json);
+    setNotes(json);
   };
+
+  // Add Note
+  const addNote = async (title, description, tag) => {
+    //TODO api call
+    const response = await fetch(`${HOST}/api/notes/addnote`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token":
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjU5Zjk3MjMxZjAxZGRmMDI0ODZlNTZkIn0sImlhdCI6MTcwNDk3NDEwMn0.3NWEnroW3nHsCsAQnPlVBAcn37PKQx182KdO7dO40JI",
+      },
+      body: JSON.stringify({ title, description, tag }),
+    });
+
+    // add note client
+    console.log("adding a new note");
+    // const note = {
+    //   _id: "65aa786bbc781bb46[added]",
+    //   user: "659f97231f01ddf02486e56d",
+    //   title: title,
+    //   description: description,
+    //   tag: tag,
+    //   date: "2024-01-17T07:50:19.520Z",
+    //   __v: 0,
+    // };
+    // setNotes(notes.concat(note));
+  };
+
   // Delete Note
   const deleteNote = (id) => {
     console.log("deleting a note with id " + id);
@@ -109,10 +57,34 @@ const NoteState = (props) => {
     });
     setNotes(newNotes);
   };
+
   // Edit Note
-  const editNote = (id, title, description, tag) => {};
+  const editNote = async (id, title, description, tag) => {
+    // Api call
+    const response = await fetch(`${HOST}/api/notes/updatenote/${id}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token":
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjU5Zjk3MjMxZjAxZGRmMDI0ODZlNTZkIn0sImlhdCI6MTcwNDk3NDEwMn0.3NWEnroW3nHsCsAQnPlVBAcn37PKQx182KdO7dO40JI",
+      },
+      body: JSON.stringify({ title, description, tag }),
+    });
+    const json = response.json();
+    // Logic to edit in client
+    for (let index = 0; index < notes.length; index++) {
+      const element = notes[index];
+      if (element._id === id) {
+        element.title = title;
+        element.description = description;
+        element.tag = tag;
+      }
+    }
+  };
   return (
-    <NoteContext.Provider value={{ notes, addNote, deleteNote, editNote }}>
+    <NoteContext.Provider
+      value={{ notes, addNote, deleteNote, editNote, getNotes }}
+    >
       {props.children}
     </NoteContext.Provider>
   );
